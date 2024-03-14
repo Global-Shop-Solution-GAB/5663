d6873e0fe7b0b0812013ef0d621076be0a374a22
Service Web <sw@gssmail.com>
2024-03-14 13:55:40 -0500

    Commit changes in C:\Websites\Git\emarlow\5663\GCG_5663_SETTINGS.sql

---
+++ GCG_5663_SETTINGS.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "GCG_5663_SETTINGS"(
+ "RemoteHost" CHAR(50),
+ "RemoteUser" CHAR(50),
+ "RemotePassword" CHAR(50),
+ "RemotePort" CHAR(50),
+ "RemotePath" CHAR(50),
+ "Weekly_Path" CHAR(50),
+ "Monthly_Path" CHAR(50));
+
+
